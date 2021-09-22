050fe070a7c2c808e1cba6210af1c79598a27176
CoalPlays <coalcrafter202@gmail.com>
2021-09-22 21:45:45 +0200

    HIHI

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from "@angular/core";
 import { timestamp } from "rxjs/operator/timestamp";
 
+interface Nav {
+  link: string;
+  name: string;
+  exact: boolean;
+}
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: ` <div class="app">
-    <passenger-viewer></passenger-viewer>
+    <nav class="nav">
+      <a
+        *ngFor="let item of nav"
+        [routerLink]="item.link"
+        routerLinkActive="active"
+        [routerLinkActiveOptions]="{ exact: item.exact }"
+        >{{ item.name }}</a
+      >
+      <a routerLink="/oops" routerLinkActive="active">404</a>
+    </nav>
+    <router-outlet></router-outlet>
   </div>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: "/",
+      name: "home",
+      exact: true,
+    },
+    {
+      link: "/oops",
+      name: "oops",
+      exact: false,
+    },
+  ];
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { CommonModule } from "@angular/common";
 
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
+import { HomeComponent } from "./home.component";
+import { NotFoundComponent } from "./not-found.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, CommonModule, PassengerDashboardModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    PassengerDashboardModule,
+    RouterModule.forRoot(routes),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
